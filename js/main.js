fd3a3cce83c00cae346c139777bfae00e7721c7c
Self-Z <alikealeksey@gmail.com>
2024-11-11 17:53:01 +0300

    update # 3 - slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(function () {
+   $('.Calculator-slider').slick({
+arrows: false
+   })
+   $('.slider-button-next').on('click', function(e){
+      e.preventDefault()
+$('.Calculator-slider').slick('slickNext')
+   })
 })
 
 

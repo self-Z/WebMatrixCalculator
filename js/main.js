6782d003ea70412337d4ea831102c845313490c1
Self-Z <alikealeksey@gmail.com>
2024-12-03 17:19:02 +0300

    Вычислительная формула вариант 2

--- js/main.js
+++ js/main.js
@@ -25,11 +25,22 @@ const number_3X3_9 = document.getElementById('number_3X3_9')
 /* ------------------------------------------------------------------------------------------------------------------------------------- */
 const creatBtn = document.getElementById('button-creat')
 const Calculator2X2Equals = document.getElementById('Calculator__2X2--equals')
+const Calculator3X3Equals = document.getElementById('Calculator-slider__3X3--equals')
 
-const Answer = number_2X2_1.value*number_2X2_4.value  - number_2X2_3.value*number_2X2_2.value 
+
 
 creatBtn.onclick = function () {
-   Calculator2X2Equals.innerHTML = `<div class="Calculator-slider_2X2--equals" id="Calculator__2X2--equals">= ${number_2X2_1.value}*${number_2X2_4.value}  - ${number_2X2_3.value}*${number_2X2_2.value}  = ${Answer} </div>`
+
+      const Answer = number_2X2_1.value * number_2X2_4.value - number_2X2_3.value * number_2X2_2.value
+      Calculator2X2Equals.innerHTML = `<div class="Calculator-slider_2X2--equals" id="Calculator__2X2--equals">= ${number_2X2_1.value}*${number_2X2_4.value}  - ${number_2X2_3.value}*${number_2X2_2.value}  = ${Answer} </div>`
+
+      
+         const Answer3x3 = number_3X3_1.value * number_3X3_5.value * number_3X3_9.value + number_3X3_4.value * number_3X3_8.value * number_3X3_3.value + number_3X3_2.value * number_3X3_6.value * number_3X3_7.value - number_3X3_7.value * number_3X3_5.value * number_3X3_3.value - number_3X3_4.value * number_3X3_2.value * number_3X3_9.value - number_3X3_8.value * number_3X3_6.value * number_3X3_1.value;
+         Calculator3X3Equals.innerHTML = `<div class="Calculator-slider_3X3--equals">= ${number_3X3_1.value}*${number_3X3_5.value}*${number_3X3_9.value} + ${number_3X3_4.value}*${number_3X3_8.value}*${number_3X3_3.value} + ${number_3X3_2.value}* ${number_3X3_6.value}* ${number_3X3_7.value} - ${number_3X3_7.value}* ${number_3X3_5.value}* ${number_3X3_3.value} - ${number_3X3_4.value}*${number_3X3_2.value}*${number_3X3_9.value} - ${number_3X3_8.value}* ${number_3X3_6.value}* ${number_3X3_1.value} = ${Answer3x3} </div>`
 }
+
+
+
+
 
 
